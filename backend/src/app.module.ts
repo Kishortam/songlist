4ab329f594b045cc6a63bda4d68673e95e1f0379
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-04-24 16:15:30 +0530

    package install in backend app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SongsModule } from './songs/songs.module';
 import { Song } from './songs/song.entity';
@@ -7,6 +8,9 @@ import { Song } from './songs/song.entity';
 // add postgres and credentials
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // So it's available everywhere
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
